6957e102986eaa86e8e76a6ec9868e6f6b765b31
Nivi Pal <nivipal@Nivis-Laptop.local>
2024-07-30 16:21:18 -0400

    updates

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { IoMenuSharp, IoHomeSharp } from 'react-icons/io5';
 import { HiDocumentText } from 'react-icons/hi';
 import { BsFillGearFill } from 'react-icons/bs';
 import { MdPhone } from 'react-icons/md';
-import { FaUser, FaFolderOpen } from 'react-icons/fa';
+import { FaUser, FaFolderOpen, FaBriefcase } from 'react-icons/fa';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CloseIcon from '@material-ui/icons/Close';
@@ -232,7 +232,21 @@ function Navbar() {
                                 </div>
                             </NavLink>
                         </Fade>
-
+                        <Fade left>
+                            <NavLink
+                                to='/#experience'
+                                smooth={true}
+                                spy='true'
+                                duration={2000}
+                            >
+                                <div className={classes.drawerItem}>
+                                <FaBriefcase className={classes.drawerIcon} />
+                                    <span className={classes.drawerLinks} >
+                                    Experience
+                                    </span>
+                                </div>
+                            </NavLink>
+                        </Fade>
                         <Fade left>
                             <NavLink
                                 to='/#skills'
--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "Howdy! 👋😄🤠 ",
-    description2: "I’m Nivishree Palvannan, and I bring enthusiasm and joy to every project I dive into. 🌟 At TORC Robotics, I'm enhancing remote assistance systems and designing intuitive interfaces for autonomous vehicles. 🚗💡 My journey includes crafting dynamic UIs with ReactJS, setting up AWS alert systems, and developing solutions for early cancer detection at Philips Healthcare. 💻🔍 With expertise in front-end technologies like AngularJS and backend solutions with Flask, I'm passionate about leveraging technology to tackle real-world challenges. Always eager to learn and tackle new projects—let’s connect and create something amazing together! 🚀🤝",
+    description2: "I’m Nivishree Palvannan, and I bring enthusiasm and joy to every project I dive into. 🌟 Currently at TORC Robotics, I'm enhancing remote assistance systems and designing intuitive interfaces for autonomous vehicles. 🚗💡 My journey includes crafting dynamic UIs, developing robust backend solutions to conducting comprehensive end-to-end testing and managing deployments. 💻🔍 With expertise in front-end technologies and backend solutions, I'm passionate about leveraging technology to tackle real-world challenges. Always eager to learn and tackle new projects—let’s connect and create something amazing together! 🚀🤝",
     image: 2
 }

9eb9f24655d8bcc9bd68eeffbc35dc73fd58afaf
Nivi Pal <nivipal@Nivis-Laptop.local>
2024-07-30 16:02:24 -0400

    changed resume

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,8 +1,8 @@
-import resume2 from '../assets/pdf/resume2.pdf'
+import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Nivishree',
     title: "Software Alchemist",
     desciption: "I’m a bundle of positivity and enthusiasm, always ready to tackle new challenges with a smile. I love finding creative solutions and making each day a bit brighter for everyone around me!",
     image: 'https://avatars.githubusercontent.com/u/90421629?s=400&u=42a60386eb41d3426cf39e07952ae9c2bbd36668&v=4',
-    resumePdf: resume2
+    resumePdf: resume
 }
